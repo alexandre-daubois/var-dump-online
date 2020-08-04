d712b9aa8ab8b453c5266b9fa9558099ee2b20c3
Alexandre Daubois <alex.daubois@gmail.com>
2020-08-04 08:23:12 +0200

    chore: clean code and style

--- src/Entity/UserVarDumpModel.php
+++ src/Entity/UserVarDumpModel.php
@@ -7,7 +7,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 class UserVarDumpModel
 {
     /**
-     * Maximum length of 128KB
+     * Maximum length of 128KB.
+     *
      * @var string
      * @Assert\Length(max="131072")
      */
